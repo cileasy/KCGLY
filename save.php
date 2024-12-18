4b4b01307553f1ce2adcff83ff05c927640ce70c
cileasy <860437738@qq.com>
2024-12-18 21:11:59 +0800

    Update save.php
    修复数据有时无法保存

--- save.php
+++ save.php
@@ -1,5 +1,10 @@
 <?php
 $data = json_decode(file_get_contents('php://input'), true);
-file_put_contents('inventoryData.json', json_encode($data));
-echo "数据保存成功";
+$success = file_put_contents('inventoryData.json', json_encode($data));
+if ($success === false) {
+    error_log("数据保存失败: ". json_last_error_msg());
+    echo "数据保存失败，请检查服务器日志获取详细信息";
+} else {
+    echo "数据保存成功";
+}
 ?>
